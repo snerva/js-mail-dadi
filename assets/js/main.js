8d2a70dda84cbefb92d0e4f2416e825396d5db73
snerva <selenenerva@live.it>
2022-10-11 16:18:41 +0200

    emails list function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,24 @@
 /*Mail*/
 //crea una lista di indirizzi, che possono accedere
+const emailsList = ['martymcfly@characters.domain', 'docbrown@characters.domain', 'georgemcfly@characters.domain', 'bifftannen@characters.domain']
 //chiedi a user la sua mail
+const userMail = prompt('Inserisci la tua email:');
 //controlla se e nella lista
- //se si stampa un messaggio in base all'esito
- //altrimenti stampa esito negativo
-
+for (let i=0; i <= emailsList.length; i++){
+    const thisEmail = emailsList[i];
+  //se si stampa un messaggio in base all'esito
+  //altrimenti stampa esito negativo
+  if (userMail != thisEmail){
+    console.log(userMail);
+    document.querySelector('h1').innerHTML = 'Indirizzo mail non valido!';
+  }else{
+    console.log(userMail);
+    document.querySelector('h1').innerHTML = 'Grande Giove!';
+  }
+}
  
+
+
 /*Gioco dei dadi*/
 //creare una variabile per pc con num casuale
   //mathrandom
