caace767876b835b24b328f24a34b2ea426c5b7c
snerva <selenenerva@live.it>
2022-10-11 19:17:17 +0200

    email true with game

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,44 +11,43 @@ for (let i=0; i <= emailsList.length; i++){
   if (userMail == thisEmail){
     console.log(userMail);
     document.querySelector('h1').innerHTML = 'Indirizzo mail valido!';
+
+    /*Gioco dei dadi*/
+    //creare una variabile per pc con num casuale
+     //mathrandom
+    const pcNumber = Math.floor(Math.random() *7);
+    console.log(pcNumber);
+    //creare una variabile per user con num casuale
+    //mathrandom
+    const userNumber = Math.floor(Math.random() *7);
+    console.log(userNumber);
+    let winner;
+    //se user ha n maggiore stampa messaggio 'hai vinto'
+    //if
+    if (pcNumber < userNumber){
+      console.log('hai vinto');
+      winner = "Hai vinto!";
+      //altrimenti stampa messaggio 'ha vinto il computer'
+      //else
+    }else{
+       console.log('ha vinto il computer');
+       winner = "Ha vinto il computer!";
+    }
+
+    const gameEl = document.querySelector("div.game");
+    let GameMarkUp = ` 
+    <div class="pc_number">
+     <h3>PC NUMBER</h3>
+     <span>${pcNumber}</span>
+    </div>
+    <div class="pc_number">
+     <h3>YOUR NUMBER</h3>
+     <span>${userNumber}</span>
+    </div>
+    <div class="winner">
+     <h2>${winner}</h2>
+    </div>`;
+    gameEl.insertAdjacentHTML("afterBegin", GameMarkUp);
+
   }
 }
- 
-/*Gioco dei dadi*/
-//creare una variabile per pc con num casuale
-  //mathrandom
-const pcNumber = Math.floor(Math.random() *7);
-console.log(pcNumber);
-//creare una variabile per user con num casuale
-  //mathrandom
-const userNumber = Math.floor(Math.random() *7);
-console.log(userNumber);
-let winner;
-//se user ha n maggiore stampa messaggio 'hai vinto'
- //if
-if (pcNumber < userNumber){
-    console.log('hai vinto');
-    winner = "Hai vinto!";
-   //altrimenti stampa messaggio 'ha vinto il computer'
- //else
-}else{
-    console.log('ha vinto il computer');
-    winner = "Ha vinto il computer!";
-}
-
-const gameEl = document.querySelector("div.game");
-let GameMarkUp = ` 
-<div class="pc_number">
-  <h3>PC NUMBER</h3>
-  <span>${pcNumber}</span>
-</div>
-<div class="pc_number">
-  <h3>YOUR NUMBER</h3>
-  <span>${userNumber}</span>
-</div>
-<div class="winner">
-  <h2>${winner}</h2>
-</div>`;
-gameEl.insertAdjacentHTML("afterBegin", GameMarkUp);
-
-           
