dd611b8eea80263f768d991657d643dd4b271f74
snerva <selenenerva@live.it>
2022-10-11 14:48:55 +0200

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+/*Mail*/
+//crea una lista di indirizzi, che possono accedere
+//chiedi a user la sua mail
+//controlla se e nella lista
+ //se si stampa un messaggio in base all'esito
+ //altrimenti stampa esito negativo
+
+ 
+/*Gioco dei dadi*/
+//creare una variabile per pc con num casuale
+  //mathrandom
+//creare una variabile per user con num casuale
+  //mathrandom
+//se user ha n maggiore stampa messaggio 'hai vinto'
+ //if
+//altrimenti stampa messaggio 'ha vinto il computer'
+ //else
